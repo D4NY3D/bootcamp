011467aeeddcc3aa516b0e93e4a91f38402c690f
DaNyXeReZ <106661035+DaNyXeReZ@users.noreply.github.com>
2022-08-02 13:04:32 +0200

    Update PrimerEjercicio.java

--- CursoJavaBasico/src/com/despinosa/PrimerEjercicio.java
+++ CursoJavaBasico/src/com/despinosa/PrimerEjercicio.java
@@ -1,3 +1,10 @@
+/*
+Crea un proyecto de Java desde 0
+
+Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
+
+Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
+*/
 package com.despinosa;
 
 public class PrimerEjercicio {
