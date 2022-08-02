33d8227c3b586055816c275a4139d09f3144c2cf
DaNyXeReZ <106661035+DaNyXeReZ@users.noreply.github.com>
2022-08-02 12:59:43 +0200

    Add files via upload

---
+++ CursoJavaBasico/src/com/despinosa/PrimerEjercicio.java
@@ -0,0 +1,20 @@
+package com.despinosa;
+
+public class PrimerEjercicio {
+
+    public static void main(String[] args) {
+        int variable1 = 5;
+        long variable2 = 65;
+        double variable3 =2.56 ;
+        String variable4 = "hola";
+        boolean variable5 = true;
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+
+        }
+    }
+
+
